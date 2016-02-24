83af846112ebdd2a09d348423a3d7f100919c3ec
soxface <sonyacorcoran@gmail.com>
2016-02-24 17:55:28 +1100

    switch to prodcution build

--- build.sh
+++ build.sh
@@ -5,16 +5,16 @@ rm -rf _build/
 make html
 
 # Development
-open _build/html/index.html
+# open _build/html/index.html
 
 # Production in Github Pages
-# ruby mcq.rb
-# cd _build/html
-# touch .nojekyll
-# git init
-# git add .
-# git commit -m 'asdf'
-# git remote add origin https://github.com/${USER}/${REPO}.git
-# git checkout -b gh-pages
-# git push --force origin gh-pages
-# open http://${USER}.github.io/${REPO}
+ruby mcq.rb
+cd _build/html
+touch .nojekyll
+git init
+git add .
+git commit -m 'asdf'
+git remote add origin https://github.com/${USER}/${REPO}.git
+git checkout -b gh-pages
+git push --force origin gh-pages
+open http://${USER}.github.io/${REPO}
